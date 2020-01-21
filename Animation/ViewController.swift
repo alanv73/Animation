0dc53c0276ef1abc8ccf2d0e44560270de8126c2
Alan Van Art <alanv73@gmail.com>
2020-01-21 13:45:45 -0500

    initial commit

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var image: UIImageView!
+    var counter = 1
+    var timer = Timer()
+    
+    @IBOutlet weak var btnStartStop: UIButton!
+    
+    @objc func animate() {
+        image.image = UIImage(named: "\(counter).gif")
+        counter += 1
+        
+        if counter == 19 {
+            counter = 0
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startClick(_ sender: Any) {
+        if !timer.isValid {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.animate), userInfo: nil, repeats: true)
+            btnStartStop.titleLabel?.text = "Stop Animation"
+        } else {
+            timer.invalidate()
+            btnStartStop.titleLabel?.text = "Start Animation"
+            
+        }
+        
+    }
+    
+    
 }
 
