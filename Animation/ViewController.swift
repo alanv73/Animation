719b124688e8c6720a6650c3490f187f6ac1a335
Alan Van Art <avanart58@southhills.edu>
2020-01-22 17:05:49 -0500

    add rotate and blink animation add spring damping to slide and grow animations

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
 
     var counter = 1
     var timer = Timer()
+    var blinkTimer = Timer()
 
     @objc func animate() {
         image.image = UIImage(named: "\(counter).gif")
@@ -22,6 +23,10 @@ class ViewController: UIViewController {
         if counter == 19 {
             counter = 0
         }
+    }
+    
+    @objc func blinkAction() {
+        image.isHidden = !image.isHidden
     }
     
     override func viewDidLoad() {
@@ -40,9 +45,12 @@ class ViewController: UIViewController {
     @IBAction func slideInClick(_ sender: Any) {
         image.center = CGPoint(x: image.center.x - 500, y: image.center.y)
         
-        UIView.animate(withDuration: 2) {
-            self.image.center = CGPoint(x: self.image.center.x + 500, y: self.image.center.y)
-        }
+//        UIView.animate(withDuration: 2) {
+//            self.image.center = CGPoint(x: self.image.center.x + 500, y: self.image.center.y)
+//        }
+        
+        UIView.animate(withDuration: 1.3, delay: 0.3, usingSpringWithDamping: 0.4, initialSpringVelocity: 0.5, options: UIView.AnimationOptions.curveEaseInOut, animations: ({self.image.center = CGPoint(x: self.image.center.x + 500, y: self.image.center.y)}), completion: nil)
+        
     }
     
     @IBAction func growClick(_ sender: Any) {
@@ -53,15 +61,34 @@ class ViewController: UIViewController {
         
         image.frame = CGRect(x: 0, y:0, width: 0, height: 0)
         
-        UIView.animate(withDuration: 1) {
-            self.image.frame = CGRect(x: iCenterX - (iWidth / 2), y: iCenterY - (iHeight / 2), width: iWidth, height: iHeight)
+//        UIView.animate(withDuration: 1) {
+//            self.image.frame = CGRect(x: iCenterX - (iWidth / 2), y: iCenterY - (iHeight / 2), width: iWidth, height: iHeight)
+//        }
+        
+        UIView.animate(withDuration: 1.3, delay: 0.3, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.5, options: UIView.AnimationOptions.curveEaseInOut, animations: ({self.image.frame = CGRect(x: iCenterX - (iWidth / 2), y: iCenterY - (iHeight / 2), width: iWidth, height: iHeight)}), completion: nil)
+    }
+    
+    @IBAction func rotateClick(_ sender: Any) {
+        let rotation: CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
+        rotation.toValue = NSNumber(value: Double.pi * 2)
+        rotation.duration = 0.5
+        rotation.isCumulative = true
+        rotation.repeatCount = 1
+        image.layer.add(rotation, forKey: "rotationAnimation")
+    }
+    
+    @IBAction func blinkClick(_ sender: Any) {
+        if blinkTimer.isValid {
+            blinkTimer.invalidate()
+            image.isHidden = false
+        } else {
+            blinkTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(ViewController.blinkAction), userInfo: nil, repeats: true)
         }
     }
     
-    
     @IBAction func startClick(_ sender: Any) {
         if !timer.isValid {
-        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.animate), userInfo: nil, repeats: true)
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.animate), userInfo: nil, repeats: true)
             btnStartStop.setTitle("Stop Animation", for: [])
         } else {
             timer.invalidate()
